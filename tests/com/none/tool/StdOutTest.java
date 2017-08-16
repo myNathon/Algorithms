ab409e574e6252d69ff18a6f0e2e97cf23d5be41
Nathon <730382930@qq.com>
2017-08-16 23:32:55 +0800

    Tool class StdOut Test file

---
+++ tests/com/none/tool/StdOutTest.java
@@ -0,0 +1,111 @@
+package com.none.tool;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Nathon on 2017/8/16.
+ */
+class StdOutTest {
+    @Test
+    void printBoolean() {
+
+    }
+
+    @Test
+    void printChar() {
+
+    }
+
+    @Test
+    void printInt() {
+
+    }
+
+    @Test
+    void printLong() {
+
+    }
+
+    @Test
+    void printFloat() {
+
+    }
+
+    @Test
+    void printDouble() {
+
+    }
+
+    @Test
+    void printCharArray() {
+
+    }
+
+    @Test
+    void printString() {
+
+    }
+
+    @Test
+    void printObject() {
+
+    }
+
+    @Test
+    void println() {
+
+    }
+
+    @Test
+    void printlnBoolean() {
+
+    }
+
+    @Test
+    void printlnChar() {
+
+    }
+
+    @Test
+    void printlnInt() {
+
+    }
+
+    @Test
+    void printlnLong() {
+
+    }
+
+    @Test
+    void printlnFloat() {
+
+    }
+
+    @Test
+    void printlnDouble() {
+
+    }
+
+    @Test
+    void printlnCharArray() {
+
+    }
+
+    @Test
+    void printlnString() {
+
+    }
+
+    @Test
+    void printlnObject() {
+
+    }
+
+    @Test
+    void printf() {
+
+    }
+
+}
