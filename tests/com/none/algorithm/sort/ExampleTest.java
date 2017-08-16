46f61fba9b075d909948429cb8fd6cc2464b65fc
Nathon <730382930@qq.com>
2017-08-16 23:35:57 +0800

    Test File of Example

---
+++ tests/com/none/algorithm/sort/ExampleTest.java
@@ -0,0 +1,26 @@
+package com.none.algorithm.sort;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Nathon on 2017/8/16.
+ */
+class ExampleTest {
+    @Test
+    void sort() {
+
+    }
+
+    @Test
+    void show() {
+
+    }
+
+    @Test
+    void isSorted() {
+
+    }
+
+}
